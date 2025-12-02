5041856b64b1e8866a32f0b75cc92a743c486d98
AngelL218 <al45850@un.edu.mx>
2025-12-02 10:40:25 -0600

    avance6

--- scrip.js
+++ scrip.js
@@ -1,6 +1,26 @@
     var div = document.getElementById("perfil");
-    var isMasculino = true; // Variable booleana para controlar el estado
+    var isMasculino = true;
     
+    function cambiar() {
+        var nombreElement = document.getElementById("nombre");
+        var profesionElement = document.getElementById("profesion");
+        
+        if (isMasculino) {
+            nombreElement.textContent = "Tatsura Yamashito";
+            profesionElement.textContent = "Es una cantautora, música, productora discográfica y arreglista japonesa, pionera del género musical conocido como city pop.";
+            alert("Se ha cambiado a femenino");
 
+            isMasculino  = false;
+        }
+
+        else if (isMasculino) {
+            nombreElement.textContent = "Tatsurō Yamashita";
+            profesionElement.textContent = "Es un cantautor, músico, productor discográfico y arreglista japonés, pionero del género musical conocido como city pop.";
+            alert("Se ha cambiado a masculino");
+        }
+
+        isMasculino = !isMasculino;
+        
+}
 
 
