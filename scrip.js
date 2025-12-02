322c34ccd0514f12b6c7a83380353941cf44871a
AngelL218 <al45850@un.edu.mx>
2025-12-02 10:09:42 -0600

    Avance4

---
+++ scrip.js
@@ -0,0 +1,14 @@
+    var div = document.getElementById("perfil");
+    
+    function cambiar() {
+            if(pan == "brioche"){
+        if(formulario.jalapeño.checked == true){
+            if(formulario.champiñones.checked == true){
+                    alert("Has ganado una promoción");
+                    total = total - 20;
+        }
+    }
+ }
+    }
+
+
